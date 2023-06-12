da5fd11143558e67a70a0fc650f318c8417a85ff
Logan <hello@loganthompson.de>
2023-06-12 13:51:27 +0200

    Check if registration is open

--- checker.py
+++ checker.py
@@ -9,10 +9,18 @@ def search_website(url, target_string):
         r = requests.get(url)
        
         if not target_string in r.text:
+            # Since we navigate to the /1 Endpoint, we should automatically land on the first step of the registration
+            # If so, then "Überprüfung" should be part of the websites content
+            # If not the maintenance page will be shown and we skip this check.
+            if "Überprüfung" in r.text:
+                print(f"INFO: Register seems up")
+                send_email_notification(url, "Registration seems up", f"Registration for {url} seems to be up.")
+                break
+                
             print(f"Target string '{target_string}' not found on the page.")
 
             # Send email notification
-            send_email_notification(url)
+            send_email_notification(url, "Site Content Changed", f"Site {url} has changed its content")
             break
             
         print(f"Target string '{target_string}' found on the page.")
@@ -21,12 +29,12 @@ def search_website(url, target_string):
         time.sleep(5)
 
 
-def send_email_notification(website_url):
+def send_email_notification(website_url, title, body_msg):
     # Set up the email parameters
     sender = "PUT EMAIL HERE"  # Replace with your email address
     recipient = "PUT EMAIL HERE"  # Replace with the recipient's email address
-    subject = "Site up"
-    body = f"Site is up {website_url}"
+    subject = title
+    body = body_msg
 
     # Create the email message
     message = EmailMessage()
@@ -49,6 +57,6 @@ def send_email_notification(website_url):
 
 
 # Example usage
-website_url = "https://anmelden.deutsch-franzoesischer-interrail-pass.de/"  # Replace with your desired website URL
+website_url = "https://anmelden.deutsch-franzoesischer-interrail-pass.de/1"  # Replace with your desired website URL
 string_to_search = "Liebe Interessenten, wir haben viele Anträge erhalten, die im Moment bearbeitet werden. Sobald alle Anträge bearbeitet wurden, wird die Plattform wieder geöffnet, um neue Anträge aufzunehmen. Vielen Dank für deine Geduld."
 search_website(website_url, string_to_search)
